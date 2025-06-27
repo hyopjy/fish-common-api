c8b281ed668ce283f7c5f38b5c935da2641a2e60
steve <934415751@qq.com>
2025-06-27 22:21:25 +0800

    event

--- src/main/java/event/UserWinEvent.java
+++ src/main/java/event/UserWinEvent.java
@@ -11,6 +11,30 @@ public class UserWinEvent {
 
     String propCode;
 
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public List<Long> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(List<Long> userIds) {
+        this.userIds = userIds;
+    }
+
+    public String getPropCode() {
+        return propCode;
+    }
+
+    public void setPropCode(String propCode) {
+        this.propCode = propCode;
+    }
+
     public UserWinEvent(Long groupId, List<Long> userIds, String propCode) {
         this.groupId = groupId;
         this.userIds = userIds;
