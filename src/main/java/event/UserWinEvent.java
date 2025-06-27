c4e00d2e8aa61d5a9d3483258abcf626043c42a8
steve <934415751@qq.com>
2025-06-27 22:18:05 +0800

    event

--- src/main/java/event/UserWinEvent.java
+++ src/main/java/event/UserWinEvent.java
@@ -1,16 +1,32 @@
 package event;
 
 import java.util.List;
+import java.util.Objects;
 
 public class UserWinEvent {
 
-    UserInfoDTO userInfoDTO;
+    Long groupId;
 
-    public UserInfoDTO getUserInfoDTO() {
-        return userInfoDTO;
+    List<Long> userIds;
+
+    String propCode;
+
+    public UserWinEvent(Long groupId, List<Long> userIds, String propCode) {
+        this.groupId = groupId;
+        this.userIds = userIds;
+        this.propCode = propCode;
     }
 
-    public void setUserInfoDTO(UserInfoDTO userInfoDTO) {
-        this.userInfoDTO = userInfoDTO;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserWinEvent that = (UserWinEvent) o;
+        return Objects.equals(groupId, that.groupId) && Objects.equals(userIds, that.userIds) && Objects.equals(propCode, that.propCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupId, userIds, propCode);
     }
 }
